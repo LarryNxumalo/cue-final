fb9b57478d68cf99fdab6f89effeb33df8721bfd
Larry Nxumalo <larry.nxumalo@afrihost.com>
2020-04-20 11:19:19 +0200

    First commit for cue  dawg

--- app.js
+++
@@ -1,8 +0,0 @@
-new hoverEffect({
-    parent: document.querySelector('.distortion'),
-    intensity: 0.9,
-    image1: 'cloud.png',
-    image2: 'cloud2.jpg',
-    angle: Math.PI / 8,
-    displacementImage: "./diss.png"
-});
--- gulpfile.js
+++ gulpfile.js
@@ -30,7 +30,6 @@ gulp.task('copy', (done) => {
     gulp.src(['resources/images/**/*']).pipe(gulp.dest('dist/img'));
     gulp.src(['resources/fonts/**/*']).pipe(gulp.dest('dist/fonts'));
     gulp.src(['*.html']).pipe(gulp.dest('dist/'));
-    gulp.src(['*.js']).pipe(gulp.dest('dist/'));
 
     done();
 });
